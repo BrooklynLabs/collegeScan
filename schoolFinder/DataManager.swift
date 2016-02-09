eb44dc75fd75509467a9b0a3a5c7573273f293f0
Jonathan Yu <jonathan@jonathanyu.net>
2016-02-08 18:01:51 -0800

    Adds GitIgnore

---
+++ schoolFinder/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  schoolFinder
+//
+//  Created by Jonathan Yu on 2/8/16.
+//  Copyright © 2016 Jonathan Yu. All rights reserved.
+//
+
+import Foundation
+
+class DataManager: NSObject {
+    var url: String = "https://api.data.gov/ed/collegescorecard/v1/schools"
+    var keys = Keys()
+    
+}
--- schoolFinder/MapVC.swift
+++ schoolFinder/MapVC.swift
@@ -7,9 +7,12 @@
 //
 
 import Foundation
+import UIKit
 
 class MapViewController: UIViewController {
     
-    
+    override func viewDidLoad() {
+        <#code#>
+    }
     
 }
